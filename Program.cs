bcac989fa79c3728b223e30a9017ffe3e3b8ad21
Grigoriy <inpuct@mail.ru>
2023-05-27 23:53:59 +0300

    Second Ex

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите размерность массива: ");
+int ArrayLenght = Convert.ToInt32(Console.ReadLine());
+Random rnd = new Random();
+int[] Array = new int[ArrayLenght];
+for (int i = 0; i < ArrayLenght; i++)
+{
+    Array[i] = rnd.Next(100,999);
+}
+foreach (var item in Array)
+{
+   Console.WriteLine($"{item}"); 
+}
+Console.WriteLine($"Количество чётных чисел в массиве - {CountOfEvenNumbers(Array)} ");
+
+int CountOfEvenNumbers(int[] Array)
+{
+    int count = 0;
+    foreach (var item in Array)
+    {
+        if(item%2==0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
