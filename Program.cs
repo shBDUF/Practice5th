59d9cd018f89b4ad510e20186c82dc8c45968cc1
Grigoriy <inpuct@mail.ru>
2023-05-27 23:59:25 +0300

    Second Ex

--- Program.cs
+++ Program.cs
@@ -1,26 +1,47 @@
-﻿Console.WriteLine("Введите размерность массива: ");
+﻿// Console.WriteLine("Введите размерность массива: ");
+// int ArrayLenght = Convert.ToInt32(Console.ReadLine());
+// Random rnd = new Random();
+// int[] Array = new int[ArrayLenght];
+// for (int i = 0; i < ArrayLenght; i++)
+// {
+//     Array[i] = rnd.Next(100,999);
+// }
+// foreach (var item in Array)
+// {
+//    Console.WriteLine($"{item}"); 
+// }
+// Console.WriteLine($"Количество чётных чисел в массиве - {CountOfEvenNumbers(Array)} ");
+
+// int CountOfEvenNumbers(int[] Array)
+// {
+//     int count = 0;
+//     foreach (var item in Array)
+//     {
+//         if(item%2==0)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+
+Console.WriteLine("Введите размерность массива: ");
 int ArrayLenght = Convert.ToInt32(Console.ReadLine());
 Random rnd = new Random();
 int[] Array = new int[ArrayLenght];
+int SumOfArray = 0;
 for (int i = 0; i < ArrayLenght; i++)
 {
-    Array[i] = rnd.Next(100,999);
+    Array[i] = rnd.Next(1, 35);
 }
+Console.WriteLine("Массив - ");
 foreach (var item in Array)
 {
-   Console.WriteLine($"{item}"); 
+    Console.WriteLine($"{item}");
 }
-Console.WriteLine($"Количество чётных чисел в массиве - {CountOfEvenNumbers(Array)} ");
-
-int CountOfEvenNumbers(int[] Array)
+for (int i = 1; i < Array.Length; i += 2)
 {
-    int count = 0;
-    foreach (var item in Array)
-    {
-        if(item%2==0)
-        {
-            count++;
-        }
-    }
-    return count;
+    SumOfArray+= Array[i];
 }
+Console.WriteLine("Сумма нечётных позиций в массиве - {0}", SumOfArray);
